d037bae086c582c8cc4261991bc587f2b3ab31db
Diyorbek <you@example.com>
2022-05-11 21:57:24 +0500

    second commit

--- js/app.js
+++ js/app.js
@@ -224,4 +224,48 @@ window.addEventListener('DOMContentLoaded', () => {
       })
    }
    forms()
+// ------------SCROLL------
+   const scroling = (upSelector) => {
+      const upElem = document.querySelector(upSelector)
+      window.addEventListener('scrole', () => {
+         if(document.documentElement.scrollTop > 1650){
+            upElem.style.opacity = 0
+         } else{
+            upElem.style.opacity = 1
+         }
+      })
+
+      let links = document.querySelectorAll('[href^="#"]')
+      let speed = .4
+
+      links.forEach(link => {
+         link.addEventListener('click', function(event){
+            event.preventDefault()
+
+            let widthTop = document.documentElement.scrollTop
+            let hash = this.hash
+            let toBlock = document.querySelector(hash).getBoundingClientRect().top
+            let start = null
+
+            requestAnimationFrame(step)
+
+            function step(time){
+               if(start === null){
+                  start = time
+               } 
+               let progress = time - start
+               let r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) : Math.min(widthTop + progress/speed, widthTop + toBlock))
+               
+               document.documentElement.scrollTo(0, r)
+               if(r != widthTop + toBlock){
+                  requestAnimationFrame(step)
+               } else{
+                  location.hash = hash
+               }
+            }         
+
+         })
+      })
+   }
+   scroling('.pageup')
 })
